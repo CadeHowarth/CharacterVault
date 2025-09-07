dba42db08e34aac572c028d443cad7e4a23716bb
CadeHowarth <cadehowarth@gmail.com>
2025-09-07 13:05:51 -0600

    server setup

---
+++ server.js
@@ -0,0 +1,17 @@
+    const express = require('express')
+    const serveStatic = require('serve-static')
+    const path = require('path')
+
+    const app = express()
+
+    app.use(express.static(path.join(__dirname, 'src')))
+
+    // Handle requests for the root path and send index.html
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'src', 'index.html'))
+    })
+
+    const port = 8080; // You can choose any available port
+    app.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`)
+    })
